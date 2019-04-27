ba05f6a2df33373bf4ee926e208d8751c6062d30
Brian Illes <brianrilles@gmail.com>
2019-04-27 09:12:55 -0700

    Adds theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from "react";
 import "./App.css";
 import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-function App() {
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2962ff"
+    },
+    secondary: {
+      main: "#f44336"
+    }
+  }
+});
+
+function App(props) {
   return (
-    <div className="App">
-      <Button variant="contained" color="primary">
-        Sign Up
-      </Button>
-    </div>
+    <>
+      <MuiThemeProvider theme={theme}>
+        <Button variant="outlined" color="primary">
+          Sign Up
+        </Button>
+        <Fab size="small" variant="extended" color="primary">
+          this is an extended variant Fab
+        </Fab>
+        <Fab size="medium" variant="extended" color="primary">
+          this is an extended variant Fab
+        </Fab>
+        <Fab variant="extended" color="primary">
+          this is an extended variant Fab
+        </Fab>
+        <Button color="secondary">Secondary</Button>
+      </MuiThemeProvider>
+    </>
   );
 }
 
