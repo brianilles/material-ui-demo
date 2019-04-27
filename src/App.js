611a211ec0c876761af70ecbd2a141d121fbe0da
Brian Illes <brianrilles@gmail.com>
2019-04-27 08:24:09 -0700

    instal material-ui, first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div className="App">
-      <p>sanity</p>
+      <Button variant="contained" color="primary">
+        Sign Up
+      </Button>
     </div>
   );
 }
